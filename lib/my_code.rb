3a74329065059476535be1c86a803d4f727e6dc9
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-01 23:34:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ def map_to_square(source_array)
   source_array.map { |x| x ** 2 }
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   
 end
 
