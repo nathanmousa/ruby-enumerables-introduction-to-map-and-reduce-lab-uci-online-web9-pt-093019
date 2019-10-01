31598d3bd4d331bb34b4cf928d4318f17fe3b6ad
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-01 23:33:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,11 +15,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  if starting_point > 0
-    source_array.reduce(starting_point) { |sum, n| sum + n }
-  else
-    source_array.reduce
-  end
+  
 end
 
 def reduce_to_all_true(source_array)
