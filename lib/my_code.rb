cae86379c7767df4f7d41c6635531a8145e6d401
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-01 23:30:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,35 @@
-# My Code here....
+def map_to_negativize(source_array)
+  source_array.map { |x| x * -1 }
+end
+
+def map_to_no_change(source_array)
+  source_array.map { |x| x * 1 }
+end
+
+def map_to_double(source_array)
+  source_array.map { |x| x * 2 }
+end
+
+def map_to_square(source_array)
+  source_array.map { |x| x ** 2 }
+end
+
+def reduce_to_total(source_array, starting_point)
+  if starting_point > 0
+    source_array.reduce(starting_point) { |sum, n| sum + n }
+  else
+    source_array.reduce
+  end
+end
+
+def reduce_to_all_true(source_array)
+  if source_array.reduce(:&) == true
+    return truthy
+  else
+    return false
+  end
+end
+
+def reduce_to_any_true(source_array)
+  
+end
