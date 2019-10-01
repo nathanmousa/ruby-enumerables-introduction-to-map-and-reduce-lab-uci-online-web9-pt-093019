d7cab451f52cb90313b831ab704df6b3cf72e67d
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-01 23:38:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,8 +17,8 @@ end
 def reduce_to_total(source_array, starting_point=0)
   total = starting_point
   counter = 0
-  while counter < array.size do
-    total += array[counter]
+  while counter < source_array.size do
+    total += source_array[counter]
     counter += 1
   end
   total
