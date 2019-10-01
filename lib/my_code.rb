0f37d35b8c7d376b7d728f1cc66dc62010d3c7da
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-01 23:42:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,7 +21,7 @@ def reduce_to_total(source_array, starting_point=0)
     total += source_array[counter]
     counter += 1
   end
-  return true
+  total
 end
 
 def reduce_to_all_true(source_array)
@@ -30,7 +30,7 @@ def reduce_to_all_true(source_array)
     return false if CONDITION
     counter += 1
   end
-  true
+  return 
 end
 
 def reduce_to_any_true(source_array)
