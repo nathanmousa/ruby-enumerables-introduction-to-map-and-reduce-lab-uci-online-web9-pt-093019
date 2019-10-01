b39b27adc33d7855b36f352664b35d2c4b8db7fc
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-01 23:52:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,7 +27,7 @@ end
 def reduce_to_all_true(source_array)
   counter = 0
   while counter < source_array.size do
-    return false if false == source_array
+    return false if source_array == "false"
     counter += 1
   end
   return true
